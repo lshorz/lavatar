d82800e6f0332be15d451b85bc09ea226c8f384d
lshorz <lshzhuhai@163.com>
2017-04-25 15:34:46 +0800

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -3,13 +3,13 @@
 if (!function_exists('lavatar_img')) {
     function lavatar_img($string, $size = 120, $backgroundColor = 'FFFFFF')
     {
-        return app('lavatar')->displayAvatar($string, $size, null, $backgroundColor);
+        return app('Lavatar')->displayAvatar($string, $size, null, $backgroundColor);
     }
 }
 
 if (!function_exists('lavatar_base64')) {
     function lavatar_base64($string, $size = 120, $backgroundColor = 'FFFFFF')
     {
-        return app('lavatar')->getAvatarBase64($string, $size, null, $backgroundColor);
+        return app('Lavatar')->getAvatarBase64($string, $size, null, $backgroundColor);
     }
 }
